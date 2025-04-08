5c1bd058bb7e4c50b660c6df93a98966cde2ad3e
Fernando <fernando.decalle@gmail.com>
2025-04-08 20:03:57 +0200

    first app

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/suma")
+def suma(a: int, b: int):
+    print("hola")
+    result = a + b
+    return result
+
+@app.get("/resta")
+def suma(a: int, b: int):
+    return a-b
