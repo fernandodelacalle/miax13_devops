9e46c5fc12206f4264812a277db2997a9ed27212
Fernando <fernando.decalle@gmail.com>
2025-04-08 20:18:01 +0200

    mult add

--- test_app.py
+++ test_app.py
@@ -1,4 +1,26 @@
+from fastapi.testclient import TestClient
+from app import app
+
+
+test_client = TestClient(app)
 
 
 def test_suma():
-    pass
+    params = {'a': 1, 'b': 2}
+    response = test_client.get('/suma', params=params)
+    assert response.status_code == 200
+    assert response.json() == 3
+
+
+def test_resta():
+    params = {'a': 2, 'b': 2}
+    response = test_client.get('/resta', params=params)
+    assert response.status_code == 200
+    assert response.json() == 0
+
+
+def test_mult():
+    params = {'a': 2, 'b': 2}
+    response = test_client.get('/mult', params=params)
+    assert response.status_code == 200
+    assert response.json() == 4
